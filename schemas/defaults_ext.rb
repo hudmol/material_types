96697b82b6f230c4084dcd5960ad3e4549b6bb53
jambun <jambun@gmail.com>
2014-11-05 14:52:47 +1100

    added user preference defaults for material types

---
+++ schemas/defaults_ext.rb
@@ -0,0 +1,14 @@
+{
+  "works_of_art" => {"type" => "boolean", "required" => false},
+  "audiovisual_materials" => {"type" => "boolean", "required" => false},
+  "books" => {"type" => "boolean", "required" => false},
+  "electronic_documents" => {"type" => "boolean", "required" => false},
+  "games" => {"type" => "boolean", "required" => false},
+  "microforms" => {"type" => "boolean", "required" => false},
+  "maps" => {"type" => "boolean", "required" => false},
+  "manuscripts" => {"type" => "boolean", "required" => false},
+  "photographs" => {"type" => "boolean", "required" => false},
+  "realia" => {"type" => "boolean", "required" => false},
+  "serials" => {"type" => "boolean", "required" => false},
+
+}
